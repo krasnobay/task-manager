55fb32c04db3b1b4e1844c06c8ac59a0b7f364ac
Vasiliy Krasnobay <krasnobay@users.noreply.github.com>
2019-03-24 17:15:52 +0400

    fix

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,9 +1,9 @@
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
-require 'coveralls'
+require 'simplecov'
 
-Coveralls.wear!
+SimpleCov.start
 
 class ActiveSupport::TestCase
   include FactoryBot::Syntax::Methods
