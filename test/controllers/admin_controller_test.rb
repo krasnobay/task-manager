783dee167628824988a0bc899ad97387be6d9a05
Vasiliy Krasnobay <krasnobay@users.noreply.github.com>
2019-03-24 19:43:11 +0400

    fix rubocop

--- test/controllers/admin_controller_test.rb
+++ test/controllers/admin_controller_test.rb
@@ -1,10 +1,10 @@
 require 'test_helper'
 
 class Admin::UsersControllerTest < ActionDispatch::IntegrationTest
-  test "should get show" do
+  test 'should get show' do
     email = generate(:string) + '@' + generate(:string)
     password = generate(:string)
-    user = create(:user, {email: email, password: password})
+    user = create(:user, email: email, password: password)
     get admin_user_url(user)
     assert_response :success
   end
