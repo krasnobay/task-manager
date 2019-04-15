c02009ab7d3cf06a0b4578ec3912475f893b8452
Василий Краснобай <krasnobay@users.noreply.github.com>
2019-04-15 19:51:11 +0400

    practice 4

--- test/factories/tasks.rb
+++ test/factories/tasks.rb
@@ -1,10 +1,8 @@
 FactoryBot.define do
   factory :task do
     name { generate :string }
-    description { generate :string}
+    description { generate :string }
     author_id { create :user }
     assignee_id { create :user }
-    state { generate :string }
-    expired_at { generate :date }
   end
 end
